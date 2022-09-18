57028bd02606f9a5d2b65b7e8ea91c5cac5ebc24
Sagar Shinde <106909440+SagarShinde7@users.noreply.github.com>
2022-09-18 11:06:22 +0530

    Add files via upload
    This is a library project. You can access the books you want

---
+++ My Library - Sagar Shinde/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Main from './Main';
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+export default App;
---
+++ My Library - Sagar Shinde/src/components/Main.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+const Main=()=>{
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyDs5gbxwV1tNf3jb5H2OqlPH_6JdX99cis'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+    return(
+        <>
+        <div className="header">
+            <div className="row1">
+                <h1>A Book is a Gift you can <br/> open again and again.</h1>
+            </div>
+            <div className="row2">
+                <h2>Find Your Book</h2>
+                <div className="search">
+                    <input type="text" placeholder="Enter Your Book Name"
+                    value={search}onChange={e=>setSearch(e.target.value)}
+                    onKeyPress={searchBook}/>
+                    
+                    <button><i class="fa-solid fa-magnifying-glass"></i></button>
+                </div>
+                <img src="./images/bg2.png" alt=""/>
+            </div>
+        </div>
+        <div className="container">
+           
+           {
+            <Card book={bookData}/>
+           }
+           
+        </div>
+        </>
+    )
+}
+export default Main;
